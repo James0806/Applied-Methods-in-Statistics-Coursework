3214178d64310ae6dfd4b1271bd096bc611c7f3b
James0806 <66918363+James0806@users.noreply.github.com>
2023-01-26 12:38:35 -0500

    Add files via upload

---
+++ S361_A3_Q1.r
@@ -0,0 +1,28 @@
+library(DescTools)
+
+X <- read.table("CommercialProperties.txt")
+y <- as.numeric(X$V1[2:82])
+x1 <- as.numeric(X$V2[2:82])
+x2 <- as.numeric(X$V3[2:82])
+x3 <- as.numeric(X$V4[2:82])
+x4 <- as.numeric(X$V5[2:82])
+
+plot(x4, y)
+
+ym <- vector(mode = "numeric", length = 81)
+ym[1:81] <- mean(y)
+line1 <- lm(y ~ ym)
+line2 <- lm(y ~ x1)
+line3 <- lm(y ~ x1 + x2)
+line4 <- lm(y ~ x1 + x2 + x3)
+line5 <- lm(y ~ x1 + x2 + x3 + x4)
+linex <- lm(y ~ x3 + x4)
+
+
+confint(line5, level=0.99)
+summary(line5)
+
+
+
+ScheffeTest(line5, g = y)
+?ScheffeTest            
